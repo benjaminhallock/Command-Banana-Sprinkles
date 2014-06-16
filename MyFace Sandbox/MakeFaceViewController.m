74948287bc2b7d9be12d81c6332762b2f8bdbc62
benben <benben>
2014-06-16 16:07:45 -0500

    ben

--- MyFace Sandbox/MakeFaceViewController.m
+++ MyFace Sandbox/MakeFaceViewController.m
@@ -25,7 +25,7 @@
     self.imagePicker.delegate = self;
     self.makeFaceScrollView.delegate = self;
     self.makeFaceScrollView.maximumZoomScale = 30;
-    self.makeFaceScrollView.minimumZoomScale = 1;
+    self.makeFaceScrollView.minimumZoomScale = 5;
     self.imagePicker.allowsEditing = NO;
 
     if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
@@ -43,14 +43,11 @@
     return self.makeFaceImageView;
 }
 
-- (IBAction)onTakePhotoPressed:(id)sender
-{
-    [self presentViewController:self.imagePicker
-                       animated:YES
-                     completion:nil];
+- (IBAction)onTakePhotoPressed:(id)sender {
+    [self presentViewController:self.imagePicker animated:YES completion:nil];
 }
 
--(UIImage*)captureFullScreen:(UIImageView *) targetView
+- (IBAction)onUploadPhotoPressed:(id)sender
 {
     float scale = 1.0f/self.makeFaceScrollView.zoomScale;
     CGRect visibleRect;
@@ -58,22 +55,10 @@
     visibleRect.origin.y = self.makeFaceScrollView.contentOffset.y * scale;
     visibleRect.size.width = self.makeFaceScrollView.bounds.size.width * scale;
     visibleRect.size.height = self.makeFaceScrollView.bounds.size.height * scale;
-        CGImageRef cr = CGImageCreateWithImageInRect([targetView.image CGImage], visibleRect);
-        UIImage* cropped = [[UIImage alloc] initWithCGImage:cr];
+    CGImageRef cr = CGImageCreateWithImageInRect([self.makeFaceImageView.image CGImage], visibleRect);
+    UIImage* cropped = [[UIImage alloc] initWithCGImage:cr];
     UIImageWriteToSavedPhotosAlbum(cropped, nil, nil, nil);
-        CGImageRelease(cr);
-        return cropped;
-    self.makeFaceImageView.image = nil;
-}
-
-- (IBAction)onUploadPhotoPressed:(id)sender
-{
-    if (self.makeFaceImageView.image == nil) {
-[self presentViewController:self.imagePicker animated:YES completion:nil];
-    } else {
-    [self captureFullScreen:self.makeFaceImageView];
-        self.makeFaceImageView.image = nil;
-    }
+//    CGImageRelease(cr);
 }
 
 #pragma mark - Image Picker Controller delegate methods
@@ -82,23 +67,23 @@
     // A photo was taken/selected!
     UIImage *imageTaken = [info objectForKey:UIImagePickerControllerOriginalImage];
 
-    if (self.imagePicker.sourceType == UIImagePickerControllerSourceTypeCamera)
-    {
-        // Save the image!
-        UIImageWriteToSavedPhotosAlbum(imageTaken, nil, nil, nil);
-    }
-
-    //You can take the metadata here => info [UIImagePickerControllerMediaMetadata];
-    UIImage* imageCropped = [info objectForKey:UIImagePickerControllerEditedImage];
-
-    CGFloat side = MIN(imageTaken.size.width, imageTaken.size.height);
-    CGFloat x = imageTaken.size.width / 2 - side / 2;
-    CGFloat y = imageTaken.size.height / 2 - side / 2;
-
-    CGRect cropRect = CGRectMake(x,y,320, 410);
-    CGImageRef imageRef = CGImageCreateWithImageInRect([imageTaken CGImage], cropRect);
-    UIImage *scaledOriginal = [UIImage imageWithCGImage:imageRef scale:imageCropped.scale orientation:imageTaken.imageOrientation];
-    CGImageRelease(imageRef);
+    //    if (self.imagePicker.sourceType == UIImagePickerControllerSourceTypeCamera)
+    //    {
+    //        // Save the image!
+    //        UIImageWriteToSavedPhotosAlbum(imageTaken, nil, nil, nil);
+    //    }
+    //
+    //    //You can take the metadata here => info [UIImagePickerControllerMediaMetadata];
+    //    UIImage* imageCropped = [info objectForKey:UIImagePickerControllerEditedImage];
+    //
+    //    CGFloat side = MIN(imageTaken.size.width, imageTaken.size.height);
+    //    CGFloat x = imageTaken.size.width / 2 - side / 2;
+    //    CGFloat y = imageTaken.size.height / 2 - side / 2;
+    //
+    //    CGRect cropRect = CGRectMake(x,y,320, 410);
+    //    CGImageRef imageRef = CGImageCreateWithImageInRect([imageTaken CGImage], cropRect);
+    //    UIImage *scaledOriginal = [UIImage imageWithCGImage:imageRef scale:imageCropped.scale orientation:imageTaken.imageOrientation];
+    //    CGImageRelease(imageRef);
 
     self.makeFaceImageView.image = imageTaken;
 
