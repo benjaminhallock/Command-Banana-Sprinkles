ace7712662f11c1b8b6d7733d46b6ea2248ddfba
benben <benben>
2014-06-17 15:25:59 -0500

    keyboard

--- MyFace Sandbox/MakeFaceViewController.m
+++ MyFace Sandbox/MakeFaceViewController.m
@@ -82,13 +82,13 @@
 
 -(void)textFieldDidBeginEditing:(UITextField *)textField {
     [UIView animateWithDuration:0.2 animations:^{
-        self.textField.frame = CGRectMake(0.0f, 320.0f, 320.0f, 30.0f);
+        self.textField.frame = CGRectMake(0.0f, self.textField.frame.origin.y - 130, 320.0f, 30.0f);
     }];
 }
 
 -(void)textFieldDidEndEditing:(UITextField *)textField {
     [UIView animateWithDuration:0.2 animations:^{
-         self.textField.frame = CGRectMake(0.0f, 484.0f, 320.0f, 30.0f);
+         self.textField.frame = CGRectMake(0.0f, self.textField.frame.origin.y + 130, 320.0f, 30.0f);
     }];
 }
 
